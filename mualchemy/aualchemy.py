f4d81784b0bc440777450061a81804ec7f9faa8b
maytham9 <gmaytham@gmail.com>
2020-06-20 15:46:38 +0200

    config using enviroment variables

--- mualchemy/aualchemy.py
+++ mualchemy/aualchemy.py
@@ -2,14 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from . import configreader
-
-
-def tables(ymalfilepath):
+from os import os.getenv
+def connection_envi_var():
+    user = = os.getenv('POSTGRES_USER')
+    host = os.getenv('POSTGRES_HOST')
+    password = os.getenv('POSTGRES_PASSWORD')
+    db = os.getenv('POSTGRES_DATABASE')
+    port = os.getenv('POSTGRES_PORT')
+    return user, host, password, db, port
+def tables():
     '''
     maps a postgres ecommerce database, returns the mapped ORM form of the tables
     needs a dbconfig.yaml to work
     '''
-    user, host, password, db, port = configreader.configreader(ymalfilepath)
+    user, host, password, db, port = connection_envi_var()
     Base = automap_base()
     engine = create_engine('postgresql+psycopg2://'+user+':'+password+'@'+host+':'+port+'/'+db, echo=True)
     Base.prepare(engine, reflect=True)
